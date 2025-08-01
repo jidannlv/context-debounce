e0711fe88e7acb558635145911b460c239d348b6
JubishaPrajapati <prajapatizubisha@gmail.com>
2025-08-01 11:37:03 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { TimerButton } from "./components/TimerButton.jsx"
+
+
+function App() {
+
+  return (
+    <>
+      <TimerButton/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TimerButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/style.css';
+
+export const TimerButton = () => {
+    return (
+        <>
+            <div>
+                <button className="timer-button">
+                    Start Timer
+                </button>
+            </div>
+        </>
+    );
+}
